06f144787bdfbab0d45d14ae62fc4c57742a1619
MadCow234 <madcow234@gmail.com>
2020-02-07 11:43:43 -0600

    Comment out ready message because Heroku keeps restarting

--- src/events/ready.js
+++ src/events/ready.js
@@ -7,14 +7,20 @@ export async function run(client) {
 
         await client.user.setActivity(`${process.env.PREFIX} commands`, {type: "LISTENING"});
 
+
+        // THE FOLLOWING CODE IS COMMENTED BECAUSE HEROKU KEEPS RESTARTING EVERY 24 HOURS
+        // This was causing a ready message to be sent to the general channel every day
+        // I would ultimately like this to happen only when the bot gets re-deployed
+        // This would allow for me to put patch notes in the ready message
+
         // Actions to skip if testing the bot (like sending a message to general chat upon login)
-        if (process.env.NODE_ENV !== 'production') return;
+        // if (process.env.NODE_ENV !== 'production') return;
 
         // Find the configured 'general' channel for the server
-        const generalChannel = client.channels.find(val => val.name === 'general');
+        // const generalChannel = client.channels.find(val => val.name === 'general');
 
         // Let the server know that I am online
-        generalChannel.send(newClientReadyEmbed(client)).catch(err => log.error(err.message));
+        // generalChannel.send(newClientReadyEmbed(client)).catch(err => log.error(err.message));
 
     } catch (err) {
         log.error(`[/events/ready.js] ${err.message}`);
