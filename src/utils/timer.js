f50e7f419e0477edc966248f8d6c88fe32660adb
MadCow234 <madcow234@gmail.com>
2020-02-15 20:35:17 -0600

    Add JSDoc

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,3 +1,10 @@
+/**
+ * Promise based sleep command.
+ * Ex. await sleep(1000);
+ *
+ * @param milliseconds length of the sleep
+ * @returns {Promise<>} an empty Promise
+ */
 exports.sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
