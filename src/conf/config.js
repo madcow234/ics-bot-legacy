48aa79cdad5068f7c7fb59c9610c764daca14fd2
MadCow234 <madcow234@gmail.com>
2020-02-18 21:02:36 -0600

    Update cancel ready check thumbnail gif

--- src/conf/config.js
+++ src/conf/config.js
@@ -10,6 +10,6 @@ exports.config = {
         errorThumbnailUrl: 'https://cdn.discordapp.com/attachments/387026235458584597/390386949631901706/flickerError.gif',
         animatedIcsBotThumbnailUrl: 'https://cdn.discordapp.com/attachments/160821484770557953/673758923446157322/icsbotanimated.gif',
         readyCheckLobbyImageUrl: 'https://cdn.discordapp.com/attachments/160594618478493696/677024135326466048/ics.gif',
-        cancelReadyCheckThumbnailUrl: 'https://cdn.discordapp.com/attachments/387026235458584597/390386951557218315/dottedClose.gif'
+        cancelReadyCheckThumbnailUrl: 'https://cdn.discordapp.com/attachments/160594618478493696/679521139214647350/cancelled.gif'
     }
 };
