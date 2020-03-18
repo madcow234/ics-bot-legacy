fb622f783d153a9e9dffef00e9f37f92a2651ec8
MadCow234 <madcow234@gmail.com>
2020-03-17 21:11:46 -0500

    Modify logger to also create stackTrace and json log files

--- src/conf/logging.js
+++ src/conf/logging.js
@@ -1,6 +1,10 @@
 import winston from 'winston';
 import fs      from 'fs';
 
+const formatPattern = winston.format.printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} ${level.toUpperCase().padStart(5)}: ${message}`;
+});
+
 /**
  * Initializes the logging framework.
  *
@@ -8,36 +12,44 @@ import fs      from 'fs';
  */
 exports.initLogger = async () => {
     const NODE_ENV = process.env.NODE_ENV;
+    const logDirName = 'logs';
+    const filename = 'ics-bot-dev';
 
+    // Log to console in production because Heroku will add STDOUT to the log stream
     if (NODE_ENV === 'production') {
-        // Log to console in production because Heroku will add STDOUT to the log stream
         await setConsoleLogger();
-
-    } else if (NODE_ENV === 'development' || NODE_ENV === 'test') {
-        try {
-            // Attempt to create a /logs directory
-            await fs.mkdirSync('./logs');
-
-        } catch (err) {
-            // If the /logs directory already exists, continue
-            // Otherwise, return a console logger
-            if (err.code !== 'EEXIST') {
-                console.log(`An error has occurred that cannot be fixed (code: ${err.code})!`);
-                console.log(`Switching to console logging...`);
-                await setConsoleLogger();
-            }
-        }
-
-        const filename = NODE_ENV === 'test' ? 'ics-bot.test.log' : 'ics-bot.log';
-
-        // As long as a /logs directory exists, return a file logger
-        await setFileLogger('logs', `${filename}`);
-
-    } else {
-        console.log(`Cannot determine current Node environment! You may need to set a "process.env.NODE_ENV" environment variable or create a .env file.`);
-        console.log(`Switching to console logging...`);
-        await setConsoleLogger();
+        return;
     }
+
+    // Attempt to create a /logs directory
+    try {
+        await fs.mkdirSync(`./${logDirName}`);
+
+    } catch (err) {
+        // If there was an error and the /logs directory does not exist, return the console logger
+        if (err.code !== 'EEXIST') {
+            console.log(`An error has occurred that cannot be fixed (code: ${err.code})!`);
+            console.log(`Switching to console logging...`);
+            await setConsoleLogger();
+        }
+    }
+
+    if (NODE_ENV === 'test') {
+        await setTestLogger(logDirName, `${filename}`);
+        return;
+    }
+
+    if (NODE_ENV === 'development') {
+        await setDevelopmentLogger(logDirName, `${filename}`);
+        return;
+    }
+
+    // If we make it all the way down here, the environment is using a weird NODE_ENV variable that we can't recognize
+    // In this case, we just log some messages to the console and set the console logger
+    console.log(`Cannot determine current Node environment! You may need to set a "process.env.NODE_ENV" environment variable or create a .env file.`);
+    console.log(`Switching to console logging...`);
+    await setConsoleLogger();
+
 };
 
 const setConsoleLogger = async () => {
@@ -47,23 +59,66 @@ const setConsoleLogger = async () => {
             new winston.transports.Console({
                 name: 'console',
                 colorize: true,
-                timestamp: function () {
-                    return new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' });
-                }
+                format: winston.format.combine(
+                    winston.format.splat(),
+                    winston.format.simple(),
+                    winston.format.timestamp(),
+                    formatPattern
+                )
             }),
         ]
     });
 };
 
-const setFileLogger = async (directoryName, filename) => {
+const setTestLogger = async (directoryName, filename) => {
     winston.configure({
         transports: [
             new winston.transports.File({
                 name: 'file',
-                filename: `./${directoryName}/${filename}`,
-                timestamp: function () {
-                    return new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' });
-                }
+                filename: `./${directoryName}/${filename}.test.log`,
+                format: winston.format.combine(
+                    winston.format.splat(),
+                    winston.format.simple(),
+                    winston.format.timestamp(),
+                    formatPattern
+                )
+            })
+        ]
+    });
+};
+
+const setDevelopmentLogger = async (directoryName, filename) => {
+    winston.configure({
+        transports: [
+            new winston.transports.File({
+                name: 'json',
+                level: 'debug',
+                filename: `./${directoryName}/${filename}.json.log`,
+                format: winston.format.combine(
+                    winston.format.json()
+                )
+            }),
+            new winston.transports.File({
+                name: 'standard',
+                level: 'debug',
+                filename: `./${directoryName}/${filename}.log`,
+                format: winston.format.combine(
+                    winston.format.splat(),
+                    winston.format.simple(),
+                    winston.format.timestamp(),
+                    formatPattern
+                )
+            }),
+            new winston.transports.File({
+                name: 'stackTrace',
+                level: 'error',
+                filename: `./${directoryName}/${filename}.stackTrace.log`,
+                format: winston.format.combine(
+                    winston.format.splat(),
+                    winston.format.simple(),
+                    winston.format.timestamp(),
+                    formatPattern
+                )
             })
         ]
     });
